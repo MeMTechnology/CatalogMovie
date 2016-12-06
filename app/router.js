da86b86d3abd0dff8ff44c65c8d2737a65462d27
Matheus J. Vieira Lemes <matheus.vieira@viamondo.com.br>
2016-12-06 16:44:25 -0200

    Mudanças router.js

--- app/router.js
+++ app/router.js
@@ -1,16 +1,1 @@
 "use strict"
-
-require('getmodule');
-
-const path ="public/";
-
-module.exports = function(app) {
-    
-    app.get("/", function(req, res) {
-        res.sendFile("index.html", {"root": __dirname});
-    });
-
-    app.get("/searchScreen", function(req, res){
-        res.sendFile("searchScreen.html", {"root": __dirname});
-    });
-};
--- server.js
+++ server.js
@@ -2,18 +2,19 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
 
+const app = express();
 app.use(bodyParser.json());
-require('./app/router.js')(app);
+app.use(express.static("public"));
+
+app.get('/', function(request, response){
+    request.sendFile('index.html')
+})
 
 const port = 5100;
 const hostname = "localhost";
 
 app.listen(port, onStart());
-
-app.use(express.static("public"));
-
 function onStart() {
     console.log(`Server started at http://${hostname}:${port}`);
 };

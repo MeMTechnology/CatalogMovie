9c6c9524bf7d0bed58a2790e98df4dc389413510
matheusvieiraa <matheus_lemes1996@hotmail.com>
2016-12-07 15:08:10 -0200

    Melhoria PDF

--- public/js/source.js
+++ public/js/source.js
@@ -19,12 +19,12 @@ function getMovieById(search) {
 	doc.setFontSize(24);
 	doc.setTextColor(255,0,0);
 	doc.text(70, 20, 'Catalog Movie');
-	doc.setFontSize(18);
+	doc.setFontSize(12);
 	doc.setTextColor(0,0,0);
-	doc.text(20, 40, 'Title');//col, linha
-	doc.text(60, 40, 'Genre');
-	doc.text(100, 40, 'Language');
-	doc.text(140, 40, 'Year');
+	doc.text(20, 40, 'Title');
+	doc.text(90, 40, 'Genre');
+	doc.text(120, 40, 'Language');
+	doc.text(160, 40, 'Year');
 	
 	
 	if(movies.length === 0){
@@ -37,9 +37,9 @@ function getMovieById(search) {
 		var ano = movies[i].ano.toString();
 		
 		doc.text(20, j, movies[i].nome);
-		doc.text(60, j, movies[i].genero);
-		doc.text(100, j, movies[i].idioma);
-		doc.text(140, j, ano);
+		doc.text(90, j, movies[i].genero);
+		doc.text(120, j, movies[i].idioma);
+		doc.text(160, j, ano);
 	}
 	
 	}
